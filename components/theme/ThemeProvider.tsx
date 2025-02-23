99caf3ff3cf45b7afca2437229233099d06c800f
saneatsu <w.saneatsu@gmail.com>
2025-02-23 20:05:15 +0900

    fix: Resolve hydration mismatch error
    Error: Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:
    
    - A server/client branch `if (typeof window !== 'undefined')`.
    - Variable input such as `Date.now()` or `Math.random()` which changes each time it's called.
    - Date formatting in a user's locale which doesn't match the server.
    - External changing data without sending a snapshot of it along with the HTML.
    - Invalid HTML tag nesting.
    
    It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
    
    https://react.dev/link/hydration-mismatch
    
      ...
        <LoadingBoundary loading={null}>
          <HTTPAccessFallbackBoundary notFound={[...]} forbidden={undefined} unauthorized={undefined}>
            <HTTPAccessFallbackErrorBoundary pathname="/" notFound={[...]} forbidden={undefined} unauthorized={undefined} ...>
              <RedirectBoundary>
                <RedirectErrorBoundary router={{...}}>
                  <InnerLayoutRouter parallelRouterKey="children" url="/" tree={[...]} childNodes={Map} segmentPath={[...]} ...>
                    <Home>
                      <div className="grid grid-...">
                        <main className="flex flex-...">
                          <img>
                          <ImagePreload>
                          <ol>
                          <ThemeToggleButton>
                            <Button variant="outline" size="icon" onClick={function handleClick}>
                              <button data-slot="button" className={"inline-f..."} onClick={function handleClick}>
                                <Moon className="h-4 w-4">
                                  <svg
                                    ref={null}
                                    xmlns="http://www.w3.org/2000/svg"
                                    width={24}
                                    height={24}
                                    viewBox="0 0 24 24"
                                    fill="none"
                                    stroke="currentColor"
                                    strokeWidth={2}
                                    strokeLinecap="round"
                                    strokeLinejoin="round"
    +                               className="lucide lucide-moon h-4 w-4"
    -                               className="lucide lucide-sun h-4 w-4"
                                  >
    +                               <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z">
    -                               <circle cx="12" cy="12" r="4">
                          ...
                        ...
                    ...
    
        at throwOnHydrationMismatch (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:2659:56)
        at beginWork (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:5413:918)
        at runWithFiberInDEV (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:631:20)
        at performUnitOfWork (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:7955:97)
        at workLoopConcurrent (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:7951:58)
        at renderRootConcurrent (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:7933:71)
        at performWorkOnRoot (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:7565:175)
        at performWorkOnRootViaSchedulerTask (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_react-dom_78bccc._.js:8394:9)
        at MessagePort.performWorkUntilDeadline (http://localhost:3000/_next/static/chunks/7d341_next_dist_compiled_3d6fe6._.js:2353:64)

--- components/theme/ThemeProvider.tsx
+++ components/theme/ThemeProvider.tsx
@@ -2,7 +2,17 @@
 
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  return (
+    mounted && <NextThemeProvider {...props}>{children}</NextThemeProvider>
+  );
 }
