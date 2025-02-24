58678c21ab5d5ea7cfc809bb017edeb271cc092a
saneatsu <w.saneatsu@gmail.com>
2025-02-24 12:24:36 +0900

    chore: Configure Playwright webServer for CI

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,19 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-	await page.goto("https://playwright.dev/");
+	await page.goto("/");
 
-	// Expect a title "to contain" a substring.
-	await expect(page).toHaveTitle(/Playwright/);
+	await expect(page).toHaveTitle("Create Next App");
 });
 
-test("get started link", async ({ page }) => {
-	await page.goto("https://playwright.dev/");
+test("get learn link", async ({ page }) => {
+	await page.goto("/");
 
 	// Click the get started link.
-	await page.getByRole("link", { name: "Get started" }).click();
+	await page.getByRole("link", { name: "Learn" }).click();
 
 	// Expects page to have a heading with the name of Installation.
-	await expect(
-		page.getByRole("heading", { name: "Installation" }),
-	).toBeVisible();
+	// await expect(
+	// 	page.getByRole("heading", { name: "Installation" }),
+	// ).toBeVisible();
 });
