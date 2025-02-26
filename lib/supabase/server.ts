bc430dce575d945852779f3fc52e817b40814511
saneatsu <w.saneatsu@gmail.com>
2025-02-27 00:58:24 +0900

    feat(supabase): Refactor Supabase client setup and add server/client support

---
+++ lib/supabase/server.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { createServerClient as createClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+import { supabaseKey, supabaseUrl } from "../supabase";
+
+export async function createServerClient() {
+	const cookieStore = await cookies();
+
+	return createClient(supabaseUrl, supabaseKey, {
+		cookies: {
+			getAll() {
+				return cookieStore.getAll();
+			},
+			setAll(cookiesToSet) {
+				try {
+					for (const cookie of cookiesToSet) {
+						cookieStore.set(cookie.name, cookie.value, cookie.options);
+					}
+				} catch {
+					// The `setAll` method was called from a Server Component.
+					// This can be ignored if you have middleware refreshing
+					// user sessions.
+				}
+			},
+		},
+	});
+}
