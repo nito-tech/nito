e965f2b5db50a014fdb69ba93bb5b755a0b97461
saneatsu <w.saneatsu@gmail.com>
2025-02-28 17:18:13 +0900

    refactor: Remove unused Supabase session check from login page
    Redirection is already handled by middleware.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,6 @@
-import { redirect } from "next/navigation";
-
 import OauthLogin from "@/components/AuthForms/OauthLogin";
-import { createServerClient } from "@/lib/supabase/server";
 
 export default async function LoginPage() {
-	const supabase = createServerClient();
-
-	const {
-		data: { session },
-	} = await (await supabase).auth.getSession();
-
-	if (session) {
-		return redirect("/dashboard");
-	}
-
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen">
 			<h1 className="text-3xl font-black mb-4">Login</h1>
