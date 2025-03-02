7a63e4cff5d0e6182ec8c21f983647b8cd8c4f56
saneatsu <w.saneatsu@gmail.com>
2025-03-02 20:57:15 +0900

    chore: Update timeout for Playwright tests to 10 seconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
 	testDir: "./e2e",
+	timeout: 10 * 1000,
+
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
