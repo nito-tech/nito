4e6dc9173f9c0e4c4e60b40af08e4d894d6a8ac8
saneatsu <w.saneatsu@gmail.com>
2025-03-09 14:36:30 +0900

    feat: Add @octokit/types for improved type definitions in GitHub API integration

--- src/features/github/lib/github-api.ts
+++ src/features/github/lib/github-api.ts
@@ -1,16 +1,7 @@
 import { Octokit as _Octokit } from "@octokit/core";
 import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
-
-export type Repository = {
-	id: number;
-	name: string;
-	full_name: string;
-	private: boolean;
-	html_url: string;
-	description: string | null;
-	updated_at: string;
-	language: string | null;
-};
+import type { Api } from "@octokit/plugin-rest-endpoint-methods";
+import type { Endpoints } from "@octokit/types";
 
 const Octokit = _Octokit.plugin(restEndpointMethods);
 
@@ -20,11 +11,13 @@ const Octokit = _Octokit.plugin(restEndpointMethods);
  * @param token - The access token for authenticating to the GitHub API.
  * @returns An instance of Octokit configured with the provided token.
  */
-export function createOctokit(token: string) {
+function createOctokit(token: string): _Octokit & Api {
 	return new Octokit({
 		auth: token,
 	});
 }
+
+type GitHubUserRepository = Endpoints["GET /user/repos"]["response"]["data"][0];
 
 /**
  * Fetches the authenticated user's repositories from GitHub.
@@ -34,22 +27,24 @@ export function createOctokit(token: string) {
  */
 export async function fetchUserRepositories(
 	token: string,
-): Promise<Repository[]> {
+): Promise<GitHubUserRepository[]> {
 	const octokit = createOctokit(token);
 
 	try {
-		// Fetch the authenticated user's repositories
 		const response = await octokit.rest.repos.listForAuthenticatedUser({
 			sort: "updated",
 			per_page: 100,
 		});
 
-		return response.data as Repository[];
+		return response.data;
 	} catch (error) {
 		console.error("Error fetching repositories:", error);
 		throw error;
 	}
 }
+
+type GitHubOrganizationRepository =
+	Endpoints["GET /orgs/{org}/repos"]["response"]["data"][0];
 
 /**
  * Fetches the repositories for the specified organization from GitHub.
@@ -61,7 +56,7 @@ export async function fetchUserRepositories(
 export async function fetchOrganizationRepositories(
 	token: string,
 	org: string,
-): Promise<Repository[]> {
+): Promise<GitHubOrganizationRepository[]> {
 	const octokit = createOctokit(token);
 
 	try {
@@ -73,34 +68,15 @@ export async function fetchOrganizationRepositories(
 
 		console.log(response.data);
 
-		return response.data as Repository[];
+		return response.data;
 	} catch (error) {
 		console.error("Error fetching organization repositories:", error);
 		throw error;
 	}
 }
 
-export type CommitInfo = {
-	sha: string;
-	commit: {
-		author: {
-			name: string;
-			email: string;
-			date: string;
-		};
-		committer: {
-			name: string;
-			email: string;
-			date: string;
-		};
-		message: string;
-	};
-	html_url: string;
-	author: {
-		login: string;
-		avatar_url: string;
-	} | null;
-};
+export type GitHubCommit =
+	Endpoints["GET /repos/{owner}/{repo}/commits"]["response"]["data"][0];
 
 /**
  * Fetches the commits for the specified repository from GitHub.
@@ -114,7 +90,7 @@ export async function fetchRepositoryCommits(
 	token: string,
 	owner: string,
 	repo: string,
-): Promise<CommitInfo[]> {
+): Promise<GitHubCommit[]> {
 	const octokit = createOctokit(token);
 
 	try {
@@ -128,20 +104,18 @@ export async function fetchRepositoryCommits(
 			console.warn(`No commits found for ${owner}/${repo}.`);
 		}
 
-		return response.data as CommitInfo[];
+		console.log("✅-------------------------------");
+		console.log(response);
+		// console.log(response.data);
+
+		return response.data;
 	} catch (error) {
 		console.error(`Error fetching commits for ${owner}/${repo}:`, error);
 		throw error;
 	}
 }
 
-export type GitHubUser = {
-	id: number;
-	login: string;
-	name: string | null;
-	html_url: string;
-	avatar_url: string;
-};
+export type GitHubUser = Endpoints["GET /user"]["response"]["data"];
 
 /**
  * Fetches the authenticated user's information from GitHub.
@@ -160,15 +134,7 @@ export async function fetchUserInfo(token: string): Promise<GitHubUser | null> {
 			return null; // or handle the case as needed
 		}
 
-		const user = {
-			id: response.data.id,
-			login: response.data.login,
-			name: response.data.name,
-			html_url: response.data.html_url,
-			avatar_url: response.data.avatar_url,
-		};
-
-		return user;
+		return response.data;
 	} catch (error) {
 		console.error("Error fetching user info:", error);
 		throw error;
