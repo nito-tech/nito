9b69e50a8e1adc77d6f56f232af37ed80906bf16
saneatsu <w.saneatsu@gmail.com>
2025-03-15 18:22:02 +0900

    feat: Add GitHub client secret to environment configuration

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,9 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+	server: {
+		GITHUB_CLIENT_SECRET: z.string(),
+	},
 	client: {
 		NEXT_PUBLIC_VERCEL_URL: z.string().optional(),
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
@@ -24,8 +27,12 @@ export const env = createEnv({
 
 		/**
 		 * GitHub
+		 *
+		 * Development: https://github.com/settings/applications/2908037
+		 * Production: https://github.com/settings/applications/2921283
 		 */
 		NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+		GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 		// URI for redirecting after authentication
 		NEXT_PUBLIC_GITHUB_REDIRECT_URI:
 			process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI,
