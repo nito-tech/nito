66b128ea523356a12049aa890fb966a00bba5611
saneatsu <w.saneatsu@gmail.com>
2025-03-20 02:17:10 +0900

    feat: Update Storybook configuration to support additional story file types and add themes addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,13 @@
 import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
 
 const config: StorybookConfig = {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(ts|tsx)"],
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
 	addons: [
 		"@storybook/addon-essentials",
 		"@storybook/addon-onboarding",
 		"@chromatic-com/storybook",
 		"@storybook/experimental-addon-test",
+		"storybook-addon-themes",
 	],
 	framework: {
 		name: "@storybook/experimental-nextjs-vite",
