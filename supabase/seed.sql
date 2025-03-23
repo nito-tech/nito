1907b6b5ed4e6d144e954ede0e588ba430705c52
saneatsu <w.saneatsu@gmail.com>
2025-03-23 09:30:14 +0900

    fix(supabase): Fix a bug that prevented logging in to the DB in the local environment
    This commit modifies the SQL seed file to create test users and profiles in the Supabase database. The changes include adding additional fields for user creation, such as instance_id and timestamps for created_at and updated_at. This enhancement facilitates testing by providing a more comprehensive dataset for user-related functionalities.

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,25 +1,52 @@
--- Create user
+-- Create test users
 INSERT INTO auth.users (
+  instance_id,
   id,
+  aud,
+  role,
   email,
   encrypted_password,
   email_confirmed_at,
+  invited_at,
+  confirmation_token,
+  confirmation_sent_at,
+  recovery_token,
+  recovery_sent_at,
+  email_change_token_new,
+  email_change,
+  email_change_sent_at,
+  created_at,
+  updated_at
+) VALUES
+(
+  '00000000-0000-0000-0000-000000000000',
+  '00000000-0000-0000-0000-000000000001',
+  'authenticated',
+  'authenticated',
+  'saneatsu.wakana@gmail.com',
+  crypt('testtest', gen_salt('bf')),
+  '2025-03-22 22:22:23.105643+00',
+  null,
+  '',
+  null,
+  '',
+  null,
+  '',
+  '',
+  null,
+  '2025-03-22 22:22:23.093826+00',
+  '2025-03-22 22:22:58.384429+00'
+);
+
+-- Create test profiles
+INSERT INTO public.profiles (
+  id,
+  username,
   created_at,
   updated_at
 ) VALUES (
   '00000000-0000-0000-0000-000000000001',
-  'saneatsu.wakana@gmail.com',
-  crypt('testtest', gen_salt('bf')),
-  now(),
+  'saneatsu',
   now(),
   now()
-);
-
--- Create profile
-INSERT INTO public.profiles (
-  id,
-  username
-) VALUES (
-  '00000000-0000-0000-0000-000000000001',
-  'saneatsu'
 );
