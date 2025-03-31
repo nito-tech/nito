194f3cc970bdce4d1065dc09d08c6dbd5f997403
saneatsu <w.saneatsu@gmail.com>
2025-03-31 22:53:38 +0900

    fix(e2e): Skip processes that manipulate DB

--- e2e/account-update.spec.ts
+++ e2e/account-update.spec.ts
@@ -20,30 +20,40 @@ test.describe("Account Update", () => {
 			);
 		});
 
-		await test.step("Update user info", async () => {
-			await page.getByLabel("Username").fill("saneatsu_updated");
-			await page.getByLabel("Email").fill("saneatsu.wakana+updated@gmail.com");
-			await page.getByRole("button", { name: "Update profile" }).click();
-			await expect(
-				page.getByText("Your profile has been updated!"),
-			).toBeVisible();
-		});
-
-		await test.step("Verify updated values", async () => {
-			await page.reload();
-			await expect(page.getByLabel("Username")).toHaveValue("saneatsu_updated");
-			await expect(page.getByLabel("Email")).toHaveValue(
-				"saneatsu.wakana+updated@gmail.com",
-			);
-		});
-
-		await test.step("Revert user info", async () => {
+		await test.step("Username duplicate error is not displayed", async () => {
+			await page.getByLabel("Username").fill("saneatsuX");
 			await page.getByLabel("Username").fill("saneatsu");
-			await page.getByLabel("Email").fill("saneatsu.wakana@gmail.com");
-			await page.getByRole("button", { name: "Update profile" }).click();
+			// Confirm that a duplicate error is not displayed when a user name that is already logged in is entered.
 			await expect(
-				page.getByText("Your profile has been updated!"),
-			).toBeVisible();
+				page.getByText("Username is already taken"),
+			).not.toBeVisible();
 		});
+
+		// await test.step("Update user info", async () => {
+		// 	await username.fill("saneatsu_updated");
+		// 	await email.fill("saneatsu.wakana+updated@gmail.com");
+		// 	await page.getByRole("button", { name: "Update profile" }).click();
+		// 	await expect(
+		// 		page.getByText("Your profile has been updated!"),
+		// 	).toBeVisible();
+		// });
+
+		// await test.step("Verify updated values", async () => {
+		// 	await page.reload();
+		// 	await expect(page.getByLabel("Username")).toHaveValue("saneatsu_updated");
+		// 	await expect(page.getByLabel("Email")).toHaveValue(
+		// 		"saneatsu.wakana+updated@gmail.com",
+		// 	);
+		// });
+
+		// // For local test
+		// await test.step("Revert user info", async () => {
+		// 	await page.getByLabel("Username").fill("saneatsu");
+		// 	await page.getByLabel("Email").fill("saneatsu.wakana@gmail.com");
+		// 	await page.getByRole("button", { name: "Update profile" }).click();
+		// 	await expect(
+		// 		page.getByText("Your profile has been updated!"),
+		// 	).toBeVisible();
+		// });
 	});
 });
