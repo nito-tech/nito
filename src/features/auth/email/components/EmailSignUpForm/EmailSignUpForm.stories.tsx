cedf8a189d8aa6f4063bddf0530b006f9f2502d5
saneatsu <w.saneatsu@gmail.com>
2025-04-01 23:57:04 +0900

    feat(storybook): Add stories for EmailLogInForm and EmailSignUpForm components

---
+++ src/features/auth/email/components/EmailSignUpForm/EmailSignUpForm.stories.tsx
@@ -0,0 +1,50 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import type { Meta, StoryObj } from "@storybook/react";
+import { useTranslations } from "next-intl";
+import { FormProvider } from "react-hook-form";
+import { z } from "zod";
+
+import { createEmailSchema } from "@/components/form/EmailField/email-schema";
+import { createPasswordSchema } from "@/components/form/PasswordField/password-schema";
+import { createUsernameSchema } from "@/components/form/UsernameField/username-schema";
+import { useFormWithOnChange } from "@/hooks/useFormWithOnChange";
+
+import { EmailSignUpForm } from "./EmailSignUpForm";
+
+const meta = {
+	title: "Features/Auth/Email/EmailSignUpForm",
+	component: EmailSignUpForm,
+	parameters: {
+		layout: "centered",
+	},
+	decorators: [
+		(Story) => {
+			const t = useTranslations();
+			const schema = z.object({
+				email: createEmailSchema(t),
+				password: createPasswordSchema(t),
+				username: createUsernameSchema(t),
+			});
+			type FormValues = z.infer<typeof schema>;
+
+			const form = useFormWithOnChange<FormValues>({
+				resolver: zodResolver(schema),
+			});
+
+			return (
+				<FormProvider {...form}>
+					<Story />
+				</FormProvider>
+			);
+		},
+	],
+	tags: ["autodocs"],
+} satisfies Meta<typeof EmailSignUpForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	tags: ["code-only"],
+};
