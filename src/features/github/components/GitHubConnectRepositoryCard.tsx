f39b0afcbf197f08e89bde568b1c531bab4bd826
saneatsu <w.saneatsu@gmail.com>
2025-04-03 01:00:34 +0900

    refactor: Remove FormError component and add Bitbucket, GitHub, and GitLab icons

--- src/features/github/components/GitHubConnectRepositoryCard.tsx
+++ src/features/github/components/GitHubConnectRepositoryCard.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import React from "react";
 
 import { ConnectRepositoryCard } from "@/components/card/ConnectRepositoryCard";
-import githubSvg from "@/components/icon/github.svg";
 import { env } from "@/config/env";
+import githubSvg from "@/shared/icon/github.svg";
 import { getSiteUrl } from "@/shared/utils/url";
 
 const GITHUB_CLIENT_ID = env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
