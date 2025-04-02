980e821274b292e183c7805e0930173165971377
saneatsu <w.saneatsu@gmail.com>
2025-04-02 23:08:01 +0900

    docs: Add comment

--- src/entities/user/model/useUsername.mock.ts
+++ src/entities/user/model/useUsername.mock.ts
@@ -1,3 +1,7 @@
+// XXX:
+// This should be moved to /shared, but it remains in /entities for now
+// as moving it would cause Storybook tests to fail.
+
 import { fn } from "@storybook/test";
 
 export const mockCheckUsernameExists = fn(async (username: string) => {
--- src/entities/user/model/useUsername.ts
+++ src/entities/user/model/useUsername.ts
@@ -1,8 +1,12 @@
+// XXX:
+// This should be moved to /shared, but it remains in /entities for now
+// as moving it would cause Storybook tests to fail.
+
 "use client";
 
 import { useState } from "react";
 
-import { checkUsernameExists as checkUsernameExistsAction } from "../api/check-username-exists";
+import { checkUsernameExists as checkUsernameExistsAction } from "../../../shared/api/check-username-exists";
 
 export function useUsername() {
 	const [isLoading, setIsLoading] = useState(false);
