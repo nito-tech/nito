02b5471edb2bb35b05da7f4e80e81df9994b1965
saneatsu <w.saneatsu@gmail.com>
2025-04-13 20:51:45 +0900

    feat(docs): Add separator in docs sidebar

--- apps/docs/src/content/_meta.js
+++ apps/docs/src/content/_meta.js
@@ -1,6 +1,19 @@
 export default {
 	index: "",
 	"get-started": "",
+	"-- frontend --": {
+		type: "separator",
+		title: <div className="flex items-center gap-2">Frontend</div>,
+	},
+	architecture: "",
+	"-- backend --": {
+		type: "separator",
+		title: <div className="flex items-center gap-2">Backend</div>,
+	},
+	"-- CI/CD --": {
+		type: "separator",
+		title: <div className="flex items-center gap-2">CI/CD</div>,
+	},
 	features: "",
 	themes: "",
 	advanced: "",
